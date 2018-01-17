d4f6fcd4a0daf4c53ed930f53e33b71f793802b5
Michal Srb <michal@redhat.com>
2018-01-17 10:50:34 +0100

    Add get_maven_packages.sh

---
+++ get_maven_packages.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+
+git clone git@github.com:msrb/maven-index-checker-1.git maven-index-checker
+pushd maven-index-checker
+git checkout csv-output
+
+mvn clean verify
+pushd target/
+
+java -jar maven-index-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -f csv > ../../maven-packages
+
+popd
+popd
